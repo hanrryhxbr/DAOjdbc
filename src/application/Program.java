47bf311e56af9f760930378f6cddf759ac39fa08
Romeryto Hanrry <romerytohanrrydearaujosilva@gmail.com>
2026-09-17 11:15:08 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     static void main() {
 
         Department obj = new Department(1, "Books");
-        IO.print(obj);
+        Seller seller = new Seller(21, "Boberto", "boberto@gmail.com", new Date(), 3000.0, obj);
+
+        IO.print(seller);
     }
 }
