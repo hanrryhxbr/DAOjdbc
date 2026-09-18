ce4f65d8a38839f46e4f89c3e846725b06562bb6
Romeryto Hanrry <romerytohanrrydearaujosilva@gmail.com>
2026-09-18 08:48:50 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,9 +17,13 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         IO.println(seller);
 
-        IO.println("\n===== TEST 1: seller findById =====");
+        IO.println("\n===== TEST 2: seller findByDepartment =====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
+        list.forEach(IO::println);
+
+        IO.println("\n===== TEST 3: seller findAll =====");
+        list = sellerDao.findAll();
         list.forEach(IO::println);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -89,7 +89,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = conn.prepareStatement("SELECT seller.*,department.Name as DepName " +
+                    "FROM seller INNER JOIN department " +
+                    "ON seller.DepartmentId = department.Id " +
+                    "ORDER BY Name");
+            
+            rs = pst.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                if (dep == null) {
+                    dep = instanciateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instanciateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(pst);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
